cec84a3c54ccba35b473dba4536fbb44bb641957
Sankalp Shrivastav <123102312+ConcernedMotley@users.noreply.github.com>
2024-11-30 08:51:50 -0700

    WORK!!!

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>TESTER</h1>
-      </header>
+    <div>
+      <h1>TESTER</h1>
     </div>
   );
 }
