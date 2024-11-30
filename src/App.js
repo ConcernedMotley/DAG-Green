8a7062d7147bae47b82a59089c04f11bd6c7bc91
Sankalp Shrivastav <123102312+ConcernedMotley@users.noreply.github.com>
2024-11-30 10:02:54 -0700

    Added Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import "./styles.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-      <h1>TESTER</h1>
+      <Navbar />
     </div>
   );
 }
